3da416c44fe3e5179f5ef082299c9513958925c5
Vinny Furia <vincent.furia@spire.com>
2020-05-12 16:16:48 -0600

    Added Makefile
    Allows for cleaner build, test, and upload of package

--- clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -r .pytest_cache/ TLE_tools.egg-info/ build/ docs/_build
-find . -name __pycache__ -exec rm -r {} +
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as fp:
 
 setup(
     name='TLE-tools',
-    version='0.3.1',
+    version='0.3.2',
     description='Library to work with two-line element set files',
     license='MIT',
     author='Spire SOS Team',
